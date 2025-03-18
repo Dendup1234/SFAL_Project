60fbfbc7a1f48a66264a58797b0e3fc9461186eb
Dendup Tshering <164316548+Dendup1234@users.noreply.github.com>
2025-03-19 01:35:42 +0600

    Final Changes

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
-web: gunicorn SFAL.wsgi
+web: gunicorn SFAL.wsgi --log-file - 
+#or works good with external database
+web: python manage.py migrate && gunicorn SFAL.wsgi
--- SFAL/settings.py
+++ SFAL/settings.py
@@ -12,7 +12,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+}
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -42,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -70,6 +75,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'SFAL.wsgi.application'
+STATICSTORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Database
